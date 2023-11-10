151d475cd0995219dad5cf81f3280efe46b3f731
JUAN FELIPE USUGA ARISTIZABAL <pipeusuga48501@gmail.com>
2023-11-09 19:58:28 -0500

    Eliminamos el campo usuario en la vista contraseña

--- views/user/contraseña.php
+++ views/user/contraseña.php
@@ -11,93 +11,82 @@
 
 <!--------------------------------------------------------FORMULARIO------------------------------------------------------->
 <br/>
-    <h1 class="text-center">CAMBIO DE CONTRASEÑA</h1>
-    <div class="row">
-      <div class="col-1">
-      </div>
-      <div class="col-10">
-      <?php
-        include '../../controllers/ingresosController.php';
+<h1 class="text-center">CAMBIO DE CONTRASEÑA</h1>
+<div class="row">
+  <div class="col-1"></div>
+  <div class="col-10">
+    <?php
+      include '../../controllers/ingresosController.php';
 
-        if ($alert === "success") {
-        ?>
-          <div class="alert alert-success" role="alert">
-            La nueva contraseña ha sido guardada
-          </div>
-        <?php
-        } elseif ($alert === "danger") {
-        ?>
-          <div class="alert alert-danger" role="alert">
-          La nueva contraseña no ha sido guardada
-          </div>
-        <?php
-        } elseif ($alert === "warning") {
-          ?>
-            <div class="alert alert-warning" role="alert">
-              Complete todos los campos
-            </div>
-          <?php
-        } else {
-          ?>
-            <div class="alert alert-light" role="alert">
-             Bienvenido
-            </div>
-          <?php
-        }
+      if ($alert === "success") {
       ?>
-      <form id="formularioContraseña" method="POST">
-        <div class="card-body">
-          <div class="row">
-            <div class="col-md-6 col-sm-12">
-              <div class="form-group">
-                <label for="usuario">Nombre de Usuario:</label>
-                <input type="text" class="form-control" name="usuario">
-                </select>
-              </div>
-            </div>
-            <div class="col-md-6 col-sm-12">
-              <div class="form-group">
-                <label for="contraseña">Antigua contraseña:</label>
-                <input type="text" class="form-control" name="contraseña">
-              </div>
-            </div>
+        <div class="alert alert-success" role="alert">
+          La nueva contraseña ha sido guardada
+        </div>
+      <?php
+      } elseif ($alert === "danger") {
+      ?>
+        <div class="alert alert-danger" role="alert">
+        La nueva contraseña no ha sido guardada
+        </div>
+      <?php
+      } elseif ($alert === "warning") {
+        ?>
+          <div class="alert alert-warning" role="alert">
+            Complete todos los campos
           </div>
-          <br>        
-          <div class="row">
-            <div class="col-md-6 col-sm-12">
-              <div class="form-group">
-                <label for="nuevacontraseña">Nueva contraseña:</label>
-                <input type="text" class="form-control" name="nuevacontraseña">
-              </div>
-            </div>
-            <div class="col-md-6 col-sm-12">
-              <div class="form-group">
-              <label for="nuevacontraseña2">Confirma nueva contraseña:</label>
-                <input type="text" class="form-control" name="nuevacontraseña2">
-              </div>
-            </div>
+        <?php
+      } else {
+        ?>
+          <div class="alert alert-light" role="alert">
+           Bienvenido
           </div>
-          <br>
-          <div class="row">
-            <div class="col-md-4">
-            </div> 
-            <div class="col-md-4 col-sm-12 text-center">
-              <button type="submit" class="btn btn-outline-dark" name="btnCambiarContraseña" value="CambiarContraseña">
-                Guardar Contraseña
-              </button>
-            </div>
-            <div class="col-md-4">
+        <?php
+      }
+    ?>
+    <form id="formularioContraseña" method="POST">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-6 col-sm-12 offset-md-3">
+            <div class="form-group">
+              <label for="contraseña">Antigua contraseña:</label>
+              <input type="text" class="form-control" name="contraseña">
             </div>
           </div>
         </div>
-      </form>
+        <br>
+        <div class="row">
+          <div class="col-md-6 col-sm-12 offset-md-3">
+            <div class="form-group">
+              <label for="nuevacontraseña">Nueva contraseña:</label>
+              <input type="text" class="form-control" name="nuevacontraseña">
+            </div>
+          </div>
+        </div>
+        <br>
+        <div class="row">
+          <div class="col-md-6 col-sm-12 offset-md-3">
+            <div class="form-group">
+            <label for="nuevacontraseña2">Confirma nueva contraseña:</label>
+              <input type="text" class="form-control" name="nuevacontraseña2">
+            </div>
+          </div>
+        </div>
+        <br>
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <button type="submit" class="btn btn-outline-dark" name="btnCambiarContraseña" value="CambiarContraseña">
+              Guardar Contraseña
+            </button>
+            <br/>
+            <br/>
+          </div>
+        </div>
       </div>
-      <div class="col-1">
-      </div>
-    </div>
-
-<br/>
-<br/>
+    </form>
+  </div>
+  <div class="col-1"></div>
+</div>
 
 <?php include '../layout/footer.php'; ?>
 
